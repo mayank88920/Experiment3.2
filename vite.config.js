c38d75dad449e6f21d425308939932b6f17ec029
mayank raj <mraj88920@gmail.com>
2025-09-21 21:22:44 +0530

    next commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/Experiment3.3/',
+   base: '/Experiment3.2/',
 })
